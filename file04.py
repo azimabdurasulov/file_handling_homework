a23eff5eb198e939079ab72d2cc75facde578e84
azimabdurasulov <azimjon20050604@gmail.com>
2022-11-03 10:50:44 +0500

    file04.py

--- file04.py
+++ file04.py
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    sum = []
+    for i in data:
+        if not i.isdigit():
+            sum += [i]
+    return sum
 # Read data from file
+
+data=open('txt_file/data04.txt','r').read()
+print(main(data))
