86bd9780f7ea0ba59684f3e17171cc407d15529b
azimabdurasulov <azimjon20050604@gmail.com>
2022-11-03 12:36:50 +0500

    file10.py

--- file10.py
+++ file10.py
@@ -7,4 +7,17 @@ def main(data:str):
         int: return answer
     """
 
+    sum = '\n'.join(data).split()
+    sum.append("Computer Vision")
+
+    answer = []
+    for i in sum:
+        answer += [len(i)]
+
+    return max(answer)
+
+
 # Read data from file
+
+data = open('txt_file/data10.txt','r').read()
+print(main(data))
