36add8b30040a418ea437fc92c0126ea44cbf273
azimabdurasulov <azimjon20050604@gmail.com>
2022-11-03 10:33:47 +0500

    file02.py

--- file02.py
+++ file02.py
@@ -6,5 +6,8 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    return len(data)
 # Read data from file
+
+data=open('txt_file/data02.txt','r').read()
+print(main(data))
