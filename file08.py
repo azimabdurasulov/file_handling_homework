b50219e984b8484ffef44b57b76da7c678399198
azimabdurasulov <azimjon20050604@gmail.com>
2022-11-03 12:00:25 +0500

    file08.py

--- file08.py
+++ file08.py
@@ -6,5 +6,17 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    sum = ''.join(data).split()
+    max=0
+    
+    for i in sum:
+        if i.isdigit():
+            if max < int(i):
+                max = int(i)
+    return max
+
 
 # Read data from file
+
+data = open('txt_file/data08.txt','r').read()
+print(main(data))
